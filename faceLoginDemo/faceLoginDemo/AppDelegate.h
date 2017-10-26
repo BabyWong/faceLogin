6eed54dc45074bb2aa7ceb57d64ed95a55d51eef
hwm <huangwenmei@sfdai.com>
2017-10-26 16:08:24 +0800

    init

---
+++ faceLoginDemo/faceLoginDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  faceLoginDemo
+//
+//  Created by hwm on 2017/10/26.
+//  Copyright © 2017年 hwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ faceLoginDemo/faceLoginDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  faceLoginDemo
+//
+//  Created by hwm on 2017/10/26.
+//  Copyright © 2017年 hwm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
